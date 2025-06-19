0d02f21514aa919384761d8b5afb46c9d409128c
Tuckermorin <161224986+Tuckermorin@users.noreply.github.com>
2025-06-19 08:24:07 -0600

    Setup Next.js app for Vercel

---
+++ frontend/app/CreateYourEvent/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import CreateYourEvent from '../../src/pages/CreateYourEvent';
+
+export default function CreateEventPage() {
+  return <CreateYourEvent onBack={() => {}} />;
+}
---
+++ frontend/app/PreviewEvent/page.tsx
@@ -0,0 +1,4 @@
+'use client';
+export default function PreviewEventPage() {
+  return <div>Preview Event page placeholder</div>;
+}
